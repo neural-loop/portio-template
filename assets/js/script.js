f94de5416facf20cc297fda358877762afcc6a67
Justin Riddiough <jriddiough@gmail.com>
2025-07-08 23:13:30 -0500

    Site refactor v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,4 @@
-// Preloader
-
-function PageLoad() {
-  $("body").removeClass("hidden");
-  TweenMax.to($(".preloader-text"), 1, {
-    force3D: true,
-    opacity: 1,
-    y: 0,
-    delay: 0.2,
-    ease: Power3.easeOut,
-  });
-
-  var width = 100,
-    perfData = window.performance.timing,
-    EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
-    time = parseInt((EstimatedTime / 500) % 50) * 70;
-
-  // Percentage Increment Animation
-  var PercentageID = $("#precent"),
-    start = 1,
-    end = 100,
-    durataion = time;
-  animateValue(PercentageID, start, end, durataion);
-
-  function animateValue(id, start, end, duration) {
-    var range = end - start,
-      current = start,
-      increment = end > start ? 1 : -1,
-      stepTime = Math.abs(Math.floor(duration / range)),
-      obj = $(id);
-
-    var timer = setInterval(function () {
-      current += increment;
-      $(obj).text(current);
-      if (current === end) {
-        clearInterval(timer);
-      }
-    }, stepTime);
-  }
-  // Fading Out Loadbar on Finised
-  setTimeout(function () {
-    TweenMax.to($(".percentage, .inner"), 0.7, {
-      force3D: true,
-      opacity: 0,
-      yPercent: -101,
-      ease: Power3.easeInOut,
-    });
-    TweenMax.to($(".preloader-wrap"), 0.7, {
-      force3D: true,
-      yPercent: -150,
-      delay: 0.2,
-      ease: Power3.easeInOut,
-    });
-  }, time);
-}
 $(document).ready(function () {
-  // preloder
-  PageLoad();
-
   // change-navigation-color
   $(window).scroll(function () {
     if ($(document).scrollTop() > 200) {
@@ -83,33 +25,6 @@ $(document).ready(function () {
 
   $(".navbar-nav>li>a").on("click", function () {
     $(".navbar-collapse").collapse("hide");
-  });
-
-  // expertise slider
-  $(".expertise__slider").slick({
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true,
-        },
-      },
-    ],
   });
 
   // Prevent page jump on resume tab change
@@ -147,12 +62,6 @@ $(document).ready(function () {
     }
   );
 
-  // venobox
-  $(".venobox").venobox();
-
-  // mixitup
-  var mixer = mixitup(".portfolio__item__wrapper");
-
   // testimonial slider
   $(".testimonial__slider").slick({
     infinite: true,
@@ -177,68 +86,4 @@ $(document).ready(function () {
       },
     ],
   });
-
-  // clients slider
-  $(".clients__slider").slick({
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-
-  // contact form
-  $("#contact-form").on("submit", function (e) {
-    e.preventDefault();
-    $.ajax({
-      url: "contact.php",
-      type: "POST",
-      data: $("#contact-form").serialize(),
-      success: function (data) {
-        $("#contact-form").each(function () {
-          this.reset();
-        });
-      },
-    });
-  });
-
-  // wow js
-  new WOW().init();
-
-  // headroom
-  $(".navbar").headroom();
 });
-
-// Preloader
-function PageLoad() {
-  if ($("#preloader").length) {
-    $("#preloader")
-      .delay(100)
-      .fadeOut("slow", function () {
-        $(this).remove();
-      });
-  }
-}
